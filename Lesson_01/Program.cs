47cb0a6aec9119c35372f6bf6c2183cb231d7e4a
Alexandr Zuev <al-ad-din@yandex.ru>
2022-10-03 12:21:11 +0300

    Домашнее задание к семинару 1

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -4,8 +4,21 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+/*
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
 
-
+if(a > b)
+{
+    Console.WriteLine($"Максимальное число: {a}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {b}");
+}
+*/
 
 /* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
@@ -13,7 +26,23 @@ a = -9 b = -3 -> max = -3
 22 3 9 -> 22
 */
 
+/*
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
 
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Максимальное из трех введенных чисел: ");
+Console.WriteLine(max);
+*/
 
 /* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
 (делится ли оно на два без остатка).
@@ -22,7 +51,18 @@ a = -9 b = -3 -> max = -3
 7 -> нет
 */
 
+/*
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
+if(n % 2 == 0)
+{
+    Console.WriteLine($"Число {n} - чётное");
+}
+else{
+    Console.WriteLine($"Число {n} - нечётное");
+}
+*/
 
 /* Задача 8: Напишите программу, которая на вход принимает число (N), 
 а на выходе показывает все чётные числа от 1 до N.
@@ -30,3 +70,20 @@ a = -9 b = -3 -> max = -3
 8 -> 2, 4, 6, 8
 */
 
+/*
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+if(n <= 1)
+{
+    Console.WriteLine("Неверный ввод");
+}
+
+while(current <= n)
+{
+    Console.Write(current + ", ");
+    current = current + 2;
+}
+*/
