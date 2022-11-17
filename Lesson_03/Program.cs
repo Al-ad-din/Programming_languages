4689a5eb8075c9e7d1d54c134b70141dc85e6fe8
Alexandr Zuev <al-ad-din@yandex.ru>
2022-11-17 15:20:41 +0300

    Практическое задание к семинару № 3

---
+++ Lesson_03/Program.cs
@@ -0,0 +1,82 @@
+﻿/* Домашнее задание к семинару № 3.
+Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+/*
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не  является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным числом");
+}
+*/
+
+/* Задача 21. Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+/*
+double FinndDistns(double x, double x1, double y, double y1, double z, double z1)
+{
+    double num = 0;
+    num = Math.Pow((x1 - x), 2) + Math.Pow((y1 - y), 2) + Math.Pow((z1 - z), 2);
+    num = Math.Sqrt(num);
+    return num;
+}
+
+Console.Write("Введите координату x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z: ");
+double z = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+double findDitans = FinndDistns(x, x1, y, y1, z, z1);
+Console.WriteLine(findDitans);
+*/
+
+/* Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+/*
+void ShowCube(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        double n = Math.Pow(count, 3);
+        Console.Write(n + " ");
+        count++;
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ShowCube(number);
+*/
